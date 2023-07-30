acbd915470c7b8ec9f47f7d70bc3fc762359b529
Angelina Souchkova <Angelina.Souchkova@gmail.com>
2023-07-30 13:30:24 +0300

    Update package.json

--- tests/example.js
+++ tests/example.js
@@ -1,9 +1,5 @@
-var assert = require('chai').assert
-  , foo = 'bar'
-  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
-
-assert.typeOf(foo, 'string'); // without optional message
-assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
-assert.equal(foo, 'bar', 'foo equal `bar`');
-assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
-assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
+suite('#indexOf()', function () {
+  test('should return -1 when not present', function () {
+    assert.equal(-1, [1, 2, 3].indexOf(4));
+  });
+});
