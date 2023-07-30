c5da57c567fae7a33cd97ec65fd10bb2cce3b319
Angelina Souchkova <Angelina.Souchkova@gmail.com>
2023-07-30 12:40:34 +0300

    Done

--- tests/Unit_test_1.js
+++ tests/Unit_test_1.js
@@ -1,12 +1,4 @@
-const chai = require('chai');
-const assert = chai.assert;
-
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
-    // #1
-    test('#isNull, #isNotNull', function () {
+var assert = require('chai').assert
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isnotNull(1, '1 is not null');
-    });
-})
-})
+      assert.isNotNull(1, '1 is not null');
+
