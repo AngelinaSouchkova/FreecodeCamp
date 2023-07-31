7df1b3780e1f9d556046a1dcea559464e3bb5f75
Angelina Souchkova <Angelina.Souchkova@gmail.com>
2023-07-31 10:53:43 +0300

    The end of unit test. Part one.

--- tests/example.js
+++ tests/example.js
@@ -1,3 +1,6 @@
+var assert = require('chai').assert
+mocha.setup('tdd');
+
 suite('#indexOf()', function () {
   test('should return -1 when not present', function () {
     assert.equal(-1, [1, 2, 3].indexOf(4));
