52321fd383610b57b5f0735e598242b3ac819173
Angelina Souchkova <Angelina.Souchkova@gmail.com>
2023-08-01 20:17:37 +0300

    Zombie.js is almost done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -103,9 +103,14 @@ suite('Functional Tests with Zombie.js', function () {
   
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
+        done();
+      });
 
-      done();
     });
     
   });
